ec94e809429225c4a7f44d1942f9b4336a2a6d64
topias paljakka <topias-paljakka@hotmail.com>
2019-03-25 19:02:22 +0200

    Initial commit

---
+++ dont_die_unity/Assets/RubberBulletGun/Scripts/LookAtMouse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+        {
+            transform.LookAt(hit.point);
+        }
+    }
+}
---
+++ dont_die_unity/Assets/RubberBulletGun/Scripts/RubberBulletScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubberBulletScript : MonoBehaviour
+{
+    Rigidbody rb;
+    float speed;
+    Vector3 heading;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed = rb.velocity.magnitude;
+        heading = rb.velocity.normalized;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 averageNormal = Vector3.zero;
+
+        foreach (var contact in collision.contacts)
+        {
+            averageNormal += contact.normal;
+        }
+        averageNormal = averageNormal / collision.contactCount;
+
+        heading = Vector3.Reflect(heading, averageNormal);
+
+        rb.velocity = heading * speed;
+    }
+}
