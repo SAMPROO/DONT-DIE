402e805d2c754be41ee88a08a3f67071c441cba7
topias33 <38688365+topias33@users.noreply.github.com>
2019-04-01 15:56:52 +0300

    Added HealthBar (number)

---
+++ dont_die_unity/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public float health = 100f;
+
+    public Text healthText;
+
+    private void Start()
+    {
+        healthText.text = ((int)health).ToString();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        healthText.text = ((int)health).ToString();
+    }
+}
