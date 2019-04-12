5897074e80274d39327e2aa3aefd9cb7b290fe2e
SAMPROO <sampo.tuisku@gmail.com>
2019-04-12 15:45:45 +0300

    Map selection menu with out of range bug

---
+++ dont_die_unity/Assets/Scripts/MapLinker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MapLinker : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class Map
+    {
+        public Button buttonPrefab;
+        public string mapSceneName;
+        public bool locked = false;
+    }
+    
+    public Map[] maps;
+
+    private void Awake()
+    {
+        
+        for (int i = 0; i < maps.Length; i++)
+        {
+            if (maps[i].locked == false)
+            {
+                maps[i].buttonPrefab.transform.GetChild(0).gameObject.SetActive(false);
+                maps[i].buttonPrefab.onClick.AddListener(delegate { SingletonGameManager.Instance.LoadNextLevel(maps[i].mapSceneName); });
+            }
+            else
+            {
+                maps[i].buttonPrefab.transform.GetChild(0).gameObject.SetActive(true);
+            }
+        }
+    }
+}
