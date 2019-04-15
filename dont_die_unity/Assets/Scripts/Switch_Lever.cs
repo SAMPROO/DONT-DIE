a8adb5c1f3399281e13308d6ce63201e77fde0cd
topias33 <38688365+topias33@users.noreply.github.com>
2019-04-15 21:43:45 +0300

    Lever v2
    Almost complete redesign to allow lever to turn over 180 degrees while still limiting movement beyond min/max angle.

--- dont_die_unity/Assets/Scripts/Switch_Lever.cs
+++ dont_die_unity/Assets/Scripts/Switch_Lever.cs
@@ -26,16 +26,19 @@ public class Switch_Lever : MonoBehaviour, ISwitch
     [Range(-1, 1)] public float startPoint;
 
     public float drag = 10;
+    public float driveForce;
 
     public Rigidbody movingPart;
     private HingeJoint joint;
+
+    private float currentAngle, maxAngle;
 
     private void OnValidate()
     {
         joint = GetComponent<HingeJoint>();
 
         // set angle limits
-        float maxAngle = turnAngle / 2;
+        maxAngle = turnAngle / 2;
         joint.limits = new JointLimits
         {
             min = -maxAngle + startPoint * maxAngle,
@@ -46,14 +49,18 @@ public class Switch_Lever : MonoBehaviour, ISwitch
         movingPart.transform.localRotation = Quaternion.AngleAxis(startPoint * .5f * turnAngle, transform.up);
 
         // set damper value
-        joint.spring = new JointSpring { damper = drag };
+        joint.spring = new JointSpring { spring = driveForce,  damper = drag, targetPosition = startPoint * turnAngle };
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        int angle = (int)movingPart.transform.eulerAngles.y;
+        currentAngle += Mathf.DeltaAngle(currentAngle, movingPart.transform.localRotation.eulerAngles.y);
+
+        float angle = currentAngle - joint.limits.min;
 
         Range = angle / turnAngle;
+
+        Debug.Log("Log: " + range);
 
         if (!State && angle >= turnAngle)
         {
---
+++ dont_die_unity/Assets/Scripts/Switch_Lever_v2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch_Lever_v2 : MonoBehaviour, ISwitch
+{
+    private bool state;
+    public bool State
+    {
+        get => state;
+        private set { state = value; }
+    }
+
+    private float range;
+    public float Range
+    {
+        get => range;
+        private set { range = value; }
+    }
+
+    public event Action OnTurnOn;
+    public event Action OnTurnOff;
+
+    public float fromAngle = 0;
+    public float toAngle = 90;
+
+    public Rigidbody movingPart;
+
+    private float minAngle, maxAngle, angularRange, currentAngle;
+
+    private void OnValidate()
+    {
+        minAngle = Mathf.Min(fromAngle, toAngle);
+        maxAngle = Mathf.Max(fromAngle, toAngle);
+        angularRange = Mathf.Abs(minAngle) + Mathf.Abs(maxAngle);
+    }
+
+    private void FixedUpdate()
+    {
+        // keep track of current angle
+        currentAngle += Mathf.DeltaAngle(currentAngle, -movingPart.transform.localRotation.eulerAngles.y);
+
+        // set range output
+        Range = (currentAngle + (minAngle < 0 ? -minAngle : 0)) / angularRange;
+        Range = Mathf.Clamp(Range, 0, 1);
+
+        if (currentAngle >= maxAngle)
+        {
+            // limit rotation to maximum angle 
+            if (currentAngle > maxAngle)
+                movingPart.transform.localRotation = Quaternion.AngleAxis(maxAngle, -transform.up);
+
+            if (!State)
+            {
+                State = true;
+                OnTurnOn?.Invoke();
+            }            
+        }
+        else if (currentAngle <= minAngle)
+        {
+            // limit rotation to minimum angle 
+            if (currentAngle < minAngle)
+                movingPart.transform.localRotation = Quaternion.AngleAxis(minAngle, -transform.up);
+
+            if (State)
+            {
+                State = false;
+                OnTurnOff?.Invoke();
+            }
+        }
+    }
+}
