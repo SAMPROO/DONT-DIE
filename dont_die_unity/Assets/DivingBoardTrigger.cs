d0922ff4b97053bbb6619cd98b7156ae67add10e
SAMPROO <sampo.tuisku@gmail.com>
2019-04-17 15:57:00 +0300

    Tweeking

---
+++ dont_die_unity/Assets/DivingBoardTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DivingBoardTrigger : MonoBehaviour
+{
+    public Animator anim;
+    public float minVelocityForTrigger = 5;
+    // Start is called before the first frame update
+    
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other);
+        Debug.Log("vel: " + other.relativeVelocity.magnitude);
+        if (other.relativeVelocity.magnitude >= minVelocityForTrigger)
+        {
+            anim.SetTrigger("PlayAnimation");
+        }
+    }
+}
