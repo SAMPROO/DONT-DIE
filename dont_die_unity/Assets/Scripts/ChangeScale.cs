472449a68f08ffc261bf165b9e96a54451264feb
SAMPROO <sampo.tuisku@gmail.com>
2019-04-24 16:17:46 +0300

    Added scaling script and physics material

---
+++ dont_die_unity/Assets/Scripts/ChangeScale.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ChangeScale : MonoBehaviour
+{
+    [Header("Properties")] 
+    public float delay = 3f;
+    public bool scaleOnCollision = true;
+    public bool destoyAfterSeconds = false;
+    public float secondsToDestroy = 0f;
+
+    [Header("Scale")] 
+    public float scaleMultiplier = 1f;
+    public float timeToScale = 1f;
+    
+    private Vector3 startScale;
+    private Vector3 endScale;
+    private bool hasCollided = false;    
+
+    private void Start()
+    {
+        startScale = transform.localScale;
+        endScale = startScale * scaleMultiplier;
+        
+        if (scaleOnCollision == false)
+            StartCoroutine(Expload(delay));
+    }
+
+    private IEnumerator Expload(float delay = 0f)
+    {
+        if (delay != 0)
+            yield return new WaitForSeconds(delay);
+        
+        
+        float i = 0f;
+
+        while (i < timeToScale)
+        {
+            i += Time.deltaTime;
+            transform.localScale = Vector3.Lerp(startScale, endScale, i / timeToScale);
+            yield return null;
+        }
+
+        if (destoyAfterSeconds)
+            Destroy(gameObject, secondsToDestroy);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (scaleOnCollision && hasCollided == false)
+        {
+            hasCollided = true;
+            StartCoroutine(Expload());
+        }
+    }
+}
---
+++ dont_die_unity/Assets/Scripts/JumpPad.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+public class JumpPad : MonoBehaviour
+{
+    public float force = 0f;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Rigidbody rb = other.transform.root.GetComponent<Rigidbody>();
+
+        if (rb != null)
+            rb.AddForce(Vector3.up * force, ForceMode.VelocityChange);
+    }
+}
