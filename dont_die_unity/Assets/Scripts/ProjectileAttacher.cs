0b79d284426b7fb275664624c069ecedc29f5f5e
Iiro <pelttarii@gmail.com>
2019-04-26 20:35:59 +0300

    Syringe

--- dont_die_unity/Assets/Scripts/ProjectileAttacher.cs
+++ dont_die_unity/Assets/Scripts/ProjectileAttacher.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class ProjectileAttacher : MonoBehaviour
 {
     private bool isAttached;
+    private bool ready;
+    public float primeTime=0.3f;
 
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("I hitted " + collision.gameObject);
 
-        if(isAttached == false)
+        if(isAttached == false && ready && !collision.gameObject.CompareTag("weapon") && !collision.gameObject.CompareTag("projectile"))
             DoAttach(collision.gameObject, collision.GetContact(0).point);
     }
 
     private void DoAttach(GameObject go,Vector3 hitPos)
     {
+        Debug.Log("I got attached to "+go);
         FixedJoint instance = gameObject.AddComponent<FixedJoint>();
-        if(go.GetComponent<Rigidbody>()==null)
+        if(go.GetComponent<Rigidbody>()!=null)
         {
             instance.connectedBody = go.GetComponent<Rigidbody>();
         }
@@ -27,5 +30,17 @@ public class ProjectileAttacher : MonoBehaviour
         }
         isAttached = true;
         
+    }
+
+    private void Update()
+    {
+        if (primeTime > 0)
+        {
+            primeTime -= 0.5f * Time.deltaTime;
+        }
+        else
+        {
+            ready = true;
+        }
     }
 }
