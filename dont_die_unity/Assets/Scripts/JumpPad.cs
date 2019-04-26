72bf3df6fccc5b59adfb8e6e99d7058134b9c7ce
SAMPROO <sampo.tuisku@gmail.com>
2019-04-26 23:07:55 +0300

    Working version of mattress
    Created AddForce function to all rigidbodies in the character

--- dont_die_unity/Assets/Scripts/JumpPad.cs
+++ dont_die_unity/Assets/Scripts/JumpPad.cs
@@ -2,16 +2,19 @@
 public class JumpPad : MonoBehaviour
 {
     public float force = 0f;
-
+    
     private void OnTriggerEnter(Collider other)
     {
-        Rigidbody rigidbody = other.transform.root.GetComponent<Rigidbody>();
+        var ragdollRoot = other.transform.root.GetComponent<RagdollArmatureRoot>();
+        var rigidbody = other.transform.GetComponent<Rigidbody>();
 
-        if (rigidbody != null)
-        {
+        ragdollRoot?.AddUniformForce(force * transform.up, ForceMode.VelocityChange);
+        
+        if (ragdollRoot == null && rigidbody != null )
             rigidbody.AddForce(force * transform.up, ForceMode.VelocityChange);
+        
+        // Also add counter force to this only if hit something hittable
+        if (ragdollRoot != null || rigidbody != null)
             gameObject.GetComponent<Rigidbody>().AddForce(-force * transform.up, ForceMode.VelocityChange);
-        }
-            
     }
 }
