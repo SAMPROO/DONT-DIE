17b232f2f6b69580163d83fa97e1d197caf5e2ea
topias33 <38688365+topias33@users.noreply.github.com>
2019-04-30 17:02:31 +0300

    Added script to destroy a joint when switch is turned on.
    and added "rope" and stuff to crane.

---
+++ dont_die_unity/Assets/JointBreaker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointBreaker : MonoBehaviour
+{
+    public Joint joint;
+
+    public GameObject switchGameObject;
+    private ISwitch iSwitch;
+
+    private void Start()
+    {
+        iSwitch = switchGameObject.GetComponent<ISwitch>();
+
+        iSwitch.OnTurnOn += BreakJoint;
+    }
+
+    private void BreakJoint()
+    {
+        if (joint)
+        {
+            Destroy(joint);
+            joint = null;
+        }
+
+        Destroy(this);
+    }
+}
