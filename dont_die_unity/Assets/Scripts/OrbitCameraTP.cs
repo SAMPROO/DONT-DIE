4d0e5218c38f2ecb2d98a47882bce1ed2f4ce306
unknown <shophorn@protonmail.com>
2019-04-30 15:18:26 +0300

    Added ground and walls layers for camera to avoid

--- dont_die_unity/Assets/Scripts/OrbitCameraTP.cs
+++ dont_die_unity/Assets/Scripts/OrbitCameraTP.cs
@@ -9,124 +9,108 @@ public class OrbitCameraTP : MonoBehaviour
     private const float Y_ANGLE_MAX = 80;
 
     public Transform anchor;
+
     public Vector2 normalLookOffset;
     public Vector2 focusLookOffset;
-
-    public Vector3 offset;
-
-    public Vector2 sensitivity = new Vector2(1,1);
     public float cameraDistanceMax;
     public float cameraDistanceMin;
 
-    public float cameraDistanceCurrent;
+    public Vector2 sensitivity = new Vector2(1,1);
 
-    private float inputX = 0;
-    private float inputY = 0;
+    [Header("Avoid clipping")]
+    public LayerMask clippingRayMask;   // not clipping really. hiding or something....
+    public float cameraColliderRadius = 0.2f;
 
-    private bool aim;
+    private float xAngle = 0;
+    private float yAngle = 0;
+    private SmoothFloat smoothFocus = new SmoothFloat (10);
 
     private IInputController input;
     public void SetInputController(IInputController input)
         => this.input = input;
 
     // For PlayerCharacter
-    public Quaternion BaseRotation => Quaternion.Euler(0, inputX, 0);
+    public Quaternion BaseRotation => Quaternion.Euler(0, xAngle, 0);
     public float AimAngle { get; private set; }
-
-    // Focus lerp things
-    private int smoothIndex = 0;
-    private const int smoothArraySize = 20;
-    private float [] smoothArray = new float[smoothArraySize];
-    private float focusLerp;
 
     public Camera GetCamera() => GetComponent<Camera>();
 
-    private Vector3 DEBUGCameraPosition;
-    private Vector3 DEBUGRayOrigin;
-    private Vector3 DEBUGRayDirection;
-
-
-    private Vector3 [] DEBUGRayPoints = new Vector3[3];
-
-    public LayerMask cameraRayMask;
-    public float cameraColliderRadius = 0.2f;
 
     private void LateUpdate()
     {
-        // Input is fine in LateUpdate
-        inputX += input.LookHorizontal * sensitivity.x *90* Time.deltaTime;
-        inputY += input.LookVertical * sensitivity.y *90* Time.deltaTime;
-        inputY = Mathf.Clamp(inputY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+        // Input stuff --------------------------------------------------------
+        xAngle += input.LookHorizontal * sensitivity.x * 90 * Time.deltaTime;
+        yAngle += input.LookVertical * sensitivity.y * 90 * Time.deltaTime;
+        yAngle = Mathf.Clamp(yAngle, Y_ANGLE_MIN, Y_ANGLE_MAX);
 
-        aim = input.Focus;
+        bool aim = input.Focus;
+        smoothFocus.Put(aim ? 1f : 0f);
 
-        smoothArray [smoothIndex] = input.Focus == true ? 1f : 0f; // == 0 / 1
-        smoothIndex = (smoothIndex + 1) % smoothArray.Length;
-        focusLerp = smoothArray.Average();
+        /// Aim stuff ---------------------------------------------------------
+        
+        // TODO: what do these numbers do? Commented line below yields same result
+        float angle;
+        angle = ((yAngle + 10) * 3f) / 300;
+        angle -= 0.2f;
+        angle = Mathf.Clamp(angle, 0, 1);
 
-        /// Aim stuff -------------------------------------------------------
-        if (aim == false)
-        {
-            float angle;
-            angle = ((inputY + 10) * 3f) / 300;
-            angle -= 0.2f;
-            angle = Mathf.Clamp(angle, 0, 1);
+        // float angle = Mathf.Clamp01((yAngle + 10) / 100f - 0.2f);
 
-            cameraDistanceCurrent = Mathf.Lerp(cameraDistanceCurrent, Mathf.Lerp(cameraDistanceMin, cameraDistanceMax, angle), 0.5f);
-        }
-        else
-        {
-            cameraDistanceCurrent = Mathf.Lerp(cameraDistanceCurrent, cameraDistanceMin / 2, 0.5f);
-        }
+        float noAimTargetDistance = Mathf.Lerp(cameraDistanceMin, cameraDistanceMax, angle);
+        float yesAimTargetDistance = cameraDistanceMin / 2f;
 
+        // Compute aim value for player controller hands.
         var flatForwardVector = new Vector3(transform.forward.x, 0, transform.forward.z);
         AimAngle = Vector3.SignedAngle(transform.forward, flatForwardVector, transform.right);
 
+        // Move stuff ---------------------------------------------------------
+        Quaternion rotation = Quaternion.Euler(yAngle, xAngle, 0);
 
-        // Move stuff ------------------------------------------------------
-        Vector2 offset = Vector2.Lerp(normalLookOffset, focusLookOffset, focusLerp);
-        Vector3 xyPosition = new Vector3(offset.x, offset.y, 0);
+        // Take smoothed focus input into account
+        float focusLerp = smoothFocus.Value;
+        float xLocalPos = Mathf.Lerp(normalLookOffset.x, focusLookOffset.x, focusLerp);
+        float yLocalPos = Mathf.Lerp(normalLookOffset.y, focusLookOffset.y, focusLerp);
+        float zLocalPos = Mathf.Lerp(noAimTargetDistance, yesAimTargetDistance, focusLerp);
 
+        /*
+        Use Physics.SphereCast to avoid going inside/behind walls. We do not currently care
+        about upward clipping, but it could be done easily.
 
-        Vector3 zPosition = new Vector3(0, 0, -cameraDistanceCurrent);
-
-        Quaternion rotation = Quaternion.Euler(inputY, inputX, 0);
-        Quaternion posRotation = rotation;//Quaternion.Euler(Mathf.Max(0, inputY), inputX, 0);
+        First, move position up. Then cast right to get x offset / local position.
+        Then from that position cast back to get z local position.
+        */
 
         // Go up. No avoid clipping on this direction, so no cast
         // TODO: rotating updirection may be the cause of "nod"
         // var upDir = rotation * Vector3.up;
         var upDir = Vector3.up;
-        var point0 = anchor.position + upDir * offset.y;
-        
+        var upPoint = anchor.position + upDir * yLocalPos;
+
+        // Use these for both casts 
+        RaycastHit hitInfo;
+        bool hit;
+
         // Go right/left
-        var rightDir = posRotation * Vector3.right;
-        var point1 = point0 + rightDir * offset.x;
-        RaycastHit hit1;
-        if (Physics.SphereCast(point0, cameraColliderRadius, rightDir, out hit1, offset.x, cameraRayMask))
-        {
-            point1 = point0 + rightDir * hit1.distance;
-        }
+        var rightDir = rotation * Vector3.right;
+        hit = Physics.SphereCast(upPoint, cameraColliderRadius, rightDir, out hitInfo, xLocalPos, clippingRayMask);
+        var rightPoint = upPoint + rightDir * (hit ? hitInfo.distance : xLocalPos);
 
         // Go back
-        var backDir = posRotation * Vector3.back;
-        var point2 = point1 + backDir * cameraDistanceCurrent;
-        RaycastHit hit2;
-        if (Physics.SphereCast(point1, cameraColliderRadius, backDir, out hit2, cameraDistanceCurrent, cameraRayMask))
-        {
-            point2 = point1 + backDir * hit2.distance;
-        }
+        var backDir = rotation * Vector3.back;
+        hit = Physics.SphereCast(rightPoint, cameraColliderRadius, backDir, out hitInfo, zLocalPos, clippingRayMask);
+        var backPoint = rightPoint + backDir * (hit ? hitInfo.distance : zLocalPos);
 
+        DEBUGRayPoints[0] = upPoint;
+        DEBUGRayPoints[1] = rightPoint;
+        DEBUGRayPoints[2] = backPoint;
 
-        DEBUGRayPoints[0] = point0;
-        DEBUGRayPoints[1] = point1;
-        DEBUGRayPoints[2] = point2;
-
-        transform.position = point2;
-
+        // Translate and rotate
+        // TODO: smooth movement here
+        transform.position = backPoint;
         transform.rotation = rotation;
     }
 
+    private Vector3 [] DEBUGRayPoints = new Vector3[3];
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
@@ -139,7 +123,5 @@ public class OrbitCameraTP : MonoBehaviour
 
         Gizmos.color = new Color(1, 0, 1);
         Gizmos.DrawWireSphere(DEBUGRayPoints[2], 0.15f);    
-
-
     }
 }
