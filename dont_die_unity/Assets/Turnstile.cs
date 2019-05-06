d4ac9af8e715b80306fa82967d1bab413821b7d3
SAMPROO <sampo.tuisku@gmail.com>
2019-05-06 15:05:32 +0300

    Turnstile basic

---
+++ dont_die_unity/Assets/Turnstile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Turnstile : MonoBehaviour
+{
+    public float force;
+
+    private HingeJoint hingeJoint;
+
+    private void Start()
+    {
+        hingeJoint = GetComponent<HingeJoint>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            var ragdollRoot = contact.otherCollider.transform.root.GetComponent<RagdollArmatureRoot>();
+            var rigidbody = contact.otherCollider.transform.GetComponent<Rigidbody>();
+
+            int dir = hingeJoint.velocity < 0 ? 1 : -1;
+
+            ragdollRoot?.AddUniformForce(force * contact.thisCollider.transform.right * dir, ForceMode.VelocityChange);
+
+            if (ragdollRoot == null && rigidbody != null)
+                rigidbody.AddForce(force * contact.thisCollider.transform.right * dir, ForceMode.VelocityChange);
+        }
+    }
+}
