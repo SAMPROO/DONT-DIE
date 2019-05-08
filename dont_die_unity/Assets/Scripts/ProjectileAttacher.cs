0da2c66ad0c36efb528b1dd983d68d98a52cdd28
unknown <shophorn@protonmail.com>
2019-05-08 13:45:02 +0300

    Added syringe, status controller

---
+++ dont_die_unity/Assets/Scripts/ProjectileAttacher.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileAttacher : MonoBehaviour
+{
+    public int effectTicks = 5;
+    public float effectPower = 20;
+    [SerializeField]
+    private Effects.Type myType;
+    private bool isAttached;
+    private bool ready;
+    public float primeTime=0.3f;
+    public AudioClip sound;
+    private GameObject go;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+    	// No need to check tags: Projectiles are on layer that doesn't hit weapons or projectiles anyway
+        if(isAttached == false && ready && !collision.gameObject.CompareTag("weapon") && !collision.gameObject.CompareTag("projectile"))
+            DoAttach(collision.gameObject, collision.GetContact(0).point);
+    }
+
+    private void DoAttach(GameObject _go,Vector3 hitPos)
+    {
+        go = _go;
+        GetComponent<AudioSource>().PlayOneShot(sound);
+        if(go.GetComponentInParent<StatusHelper>())
+        {
+            switch (myType)
+            {
+                case Effects.Type.Heal:
+                    go.GetComponentInParent<StatusHelper>().rr.OnStatusHeal(effectTicks, effectPower, true);
+                    break;
+
+                case Effects.Type.Damage:
+                    go.GetComponentInParent<StatusHelper>().rr.OnStatusDamage(effectTicks, effectPower, true);
+                    break;
+
+                case Effects.Type.Slow:
+                    go.GetComponentInParent<StatusHelper>().rr.OnStatusSlow(effectTicks, effectPower, true);
+                    break;
+
+                case Effects.Type.Stun:
+                    go.GetComponentInParent<StatusHelper>().rr.OnStatusStun(effectTicks, true);
+                    break;
+            }
+            
+        }
+
+        FixedJoint instance = gameObject.AddComponent<FixedJoint>();
+        if(go.GetComponent<Rigidbody>()!=null)
+        {
+            instance.connectedBody = go.GetComponent<Rigidbody>();
+        }
+        else
+        {
+            instance.connectedAnchor = hitPos;
+        }
+        isAttached = true;
+        
+    }
+
+    private void Update()
+    {
+        if (primeTime > 0)
+        {
+            primeTime -= 0.5f * Time.deltaTime;
+        }
+        else
+        {
+            ready = true;
+        }
+    }
+}
---
+++ dont_die_unity/Assets/Scripts/StatusController.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusController : MonoBehaviour
+{
+    
+    [Header("Time between ticks in seconds")]
+    [Range(0.1f,2f)]
+    public float tickDelay=0.5f;
+    public float currentTickTime;
+    private List<StatusEffect> statusEffects = new List<StatusEffect>();
+    private float[] statusPower;
+    public PlayerController pc;
+    public RagdollRig rr;
+
+    private void Awake()
+    {
+        if(GetComponent<PlayerController>()!=null)
+            pc = GetComponent<PlayerController>();
+
+        if (GetComponent<PlayerController>() != null)
+            rr = GetComponent<RagdollRig>();
+    }
+    private void Update()
+    {
+        ApplyStatus();
+    }
+
+    public void OnStatusHeal(int ticks, float amount, bool isReset)
+    {
+        StatusEffect status = new StatusEffect(Effects.Type.Heal, ticks, amount, pc, rr);
+        statusEffects.Add(status);
+        status.OnApply();
+    }
+    public void OnStatusDamage(int ticks, float amount, bool isReset)
+    {
+        StatusEffect status = new StatusEffect(Effects.Type.Damage, ticks, amount, pc, rr);
+        statusEffects.Add(status);
+        status.OnApply();
+    }
+    public void OnStatusSlow(int ticks, float amount, bool isReset)
+    {
+
+    }
+    public void OnStatusStun(int ticks, bool isReset)
+    {
+
+    }
+
+    public void ApplyStatus()
+    {
+        if (currentTickTime < tickDelay)
+            currentTickTime += 1 * Time.deltaTime;
+
+        else
+        {
+            Debug.Log(statusEffects.Count+ "this many status effetcs");
+            for (int i = statusEffects.Count - 1; i >= 0; i--)
+            {
+                if(statusEffects[i]!=null)
+                {
+                    if (statusEffects[i].myTicks <= 0)
+                    {
+                        statusEffects[i].Release();
+                        statusEffects.RemoveAt(i);
+                    }
+
+                    else
+                    {
+                        statusEffects[i].DoEffect();
+                        statusEffects[i].myTicks -= 1;
+                    }
+
+                    
+                }
+                
+            }
+            currentTickTime = 0;
+        }
+    }
+}
+public class StatusEffect
+{
+    private Effects.Type myType;
+    public int myTicks;
+    public float myPower;
+    PlayerController pc;
+    RagdollRig rr;
+
+    public StatusEffect(Effects.Type _myType, int _ticks, float _power, PlayerController _pc, RagdollRig _rr)
+    {
+        pc = _pc;
+        rr = _rr;
+        myType = _myType;
+        myPower = _power;
+        myTicks = _ticks;
+    }
+
+    public void DoEffect()
+    {
+        switch(myType)
+        {
+            case Effects.Type.Heal:
+                rr.healVFX.gameObject.SetActive(true);
+                rr.healVFX.Play(true);
+                pc.Hurt(-myPower);
+                break;
+
+            case Effects.Type.Damage:
+                rr.damageVFX.gameObject.SetActive(true);
+                rr.damageVFX.Play(true);
+                pc.Hurt(myPower);
+                break;
+
+            case Effects.Type.Slow:
+
+                break;
+
+            case Effects.Type.Stun:
+
+                break;
+        }
+    }
+    public void Release()
+    {
+        myTicks = 0;
+        switch (myType)
+        {
+            case Effects.Type.Heal:
+                rr.healVFX.gameObject.SetActive(false);
+                pc.Hurt(-myPower);
+                break;
+
+            case Effects.Type.Damage:
+                rr.damageVFX.gameObject.SetActive(false);
+                pc.Hurt(myPower);
+                break;
+
+            case Effects.Type.Slow:
+
+                break;
+
+            case Effects.Type.Stun:
+
+                break;
+        }
+    }
+    public void OnApply()
+    {
+        Debug.Log("HeyBaby im applying, my type is "+myType);
+        switch (myType)
+        {
+            case Effects.Type.Heal:
+                rr.healVFX.gameObject.SetActive(true);
+                rr.healVFX.Play(true);
+                break;
+
+            case Effects.Type.Damage:
+                rr.damageVFX.gameObject.SetActive(true);
+                rr.damageVFX.Play(true);
+                break;
+
+            case Effects.Type.Slow:
+
+                break;
+
+            case Effects.Type.Stun:
+
+                break;
+        }
+    }
+}
+public class Effects
+{
+    public enum Type { Heal, Damage, Slow, Stun };
+}
