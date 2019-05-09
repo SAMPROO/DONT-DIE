1c7d4b02b9a77e3d3bc4d719b0d4c69c3d74c9a9
Iiro <pelttarii@gmail.com>
2019-05-10 01:18:56 +0300

    music

---
+++ dont_die_unity/Assets/MusicManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip gameMusic;
+    public AudioClip victorySound;
+
+    public void MatchStart()
+    {
+        GetComponent<AudioSource>().Stop();
+        GetComponent<AudioSource>().clip = gameMusic;
+        GetComponent<AudioSource>().loop = true;
+        GetComponent<AudioSource>().Play();
+        
+    }
+    public void MatchEnd()
+    {
+        GetComponent<AudioSource>().Stop();
+        GetComponent<AudioSource>().clip = victorySound;
+        GetComponent<AudioSource>().loop = false;
+        GetComponent<AudioSource>().Play();
+    }
+}
