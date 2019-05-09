7ac2c7292a93440ef31e7e00778ba91c542d187b
Iiro <pelttarii@gmail.com>
2019-05-10 01:25:20 +0300

    music manager fixed

--- dont_die_unity/Assets/MusicManager.cs
+++ dont_die_unity/Assets/MusicManager.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+    public AudioClip menuMusic;
     public AudioClip gameMusic;
     public AudioClip victorySound;
+    public float baseVolume=0.4f;
 
-    public void MatchStart()
+    public void PlayMenu(float _volume = 0.4f)
     {
         GetComponent<AudioSource>().Stop();
+        GetComponent<AudioSource>().volume=_volume;
+        GetComponent<AudioSource>().clip = menuMusic;
+        GetComponent<AudioSource>().loop = true;
+        GetComponent<AudioSource>().Play();
+
+    }
+
+    public void PlayStart(float _volume = 0.4f)
+    {
+        GetComponent<AudioSource>().Stop();
+        GetComponent<AudioSource>().volume = _volume;
         GetComponent<AudioSource>().clip = gameMusic;
         GetComponent<AudioSource>().loop = true;
         GetComponent<AudioSource>().Play();
         
     }
-    public void MatchEnd()
+
+    public void PlayEnd(float _volume = 0.4f)
     {
         GetComponent<AudioSource>().Stop();
+        GetComponent<AudioSource>().volume = _volume;
         GetComponent<AudioSource>().clip = victorySound;
         GetComponent<AudioSource>().loop = false;
         GetComponent<AudioSource>().Play();
